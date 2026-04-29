0bb59689980bebee95aec1eccbb3cbdf46f2a976
Mateus Chagas <mateus.chagas@avancoinfo.com.br>
2026-04-29 18:17:24 -0300

    :sparkles:feat: cria pagina de login

---
+++ src/app/features/auth/components/login-form/login-form.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormField } from '../../../../shared/components/form-field/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ButtonComponent } from '../../../../shared/components/button/button';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, FormField, ButtonComponent],
+  templateUrl: './login-form.html',
+  styleUrl: './login-form.scss',
+})
+export class LoginForm {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      console.log('Formulário inválido');
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('Formulário enviado:', this.form.value);
+
+    // chamar API aqui
+    // this.authService.login(this.form.value)
+  }
+
+  get emailControl(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get passControl(): FormControl {
+    return this.form.get('pass') as FormControl;
+  }
+}
