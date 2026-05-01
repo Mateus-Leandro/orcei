37b775f87c318b28a9af2ae4c727e8b5dcd9d0ad
Mateus Chagas <mateusleandro2205@gmail.com>
2026-05-01 12:44:20 -0300

    :lipstick:feat: ajusta layout tela principal

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -1,13 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { Spinner } from '../../shared/components/spinner/spinner';
 import { LoadingService } from '../../core/services/loading/loading.service';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [Spinner],
+  imports: [Spinner, NgClass],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
 export class AuthLayout {
   loading = inject(LoadingService).loading;
+
+  constructor(private router: Router) {}
+
+  registerPage(): boolean {
+    return this.router.url === '/register';
+  }
 }
