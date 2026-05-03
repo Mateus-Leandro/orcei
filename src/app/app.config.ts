01e3beb5ce16aa7422ac416c805437c4a5cfe95f
Mateus Chagas <mateusleandro2205@gmail.com>
2026-05-02 22:14:32 -0300

    :sparkles:feat: ajusta paginator para portugues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 import { routes } from './app.routes';
+import { MatPaginatorIntlPtBr } from './shared/helpers/mat-paginator-intl-pt-br';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideEnvironmentNgxMask(),
+    { provide: MatPaginatorIntl, useClass: MatPaginatorIntlPtBr },
   ],
 };
