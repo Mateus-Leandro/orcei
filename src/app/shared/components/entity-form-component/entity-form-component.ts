c9b3bfa79b381acc885d67b1022491f4a32be7fa
Mateus Chagas <mateus.chagas@avancoinfo.com.br>
2026-05-04 20:37:16 -0300

    :sparkles:feat: cria coponent entity form component

---
+++ src/app/shared/components/entity-form-component/entity-form-component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ContainerPageLayout } from '../../../layouts/container-page-layout/container-page-layout';
+import { MatCardModule } from '@angular/material/card';
+import { ButtonComponent } from '../button/button';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-entity-form-component',
+  imports: [ContainerPageLayout, MatCardModule, ButtonComponent, ReactiveFormsModule, MatIcon],
+  templateUrl: './entity-form-component.html',
+  styleUrl: './entity-form-component.scss',
+})
+export class EntityFormComponent {
+  @Output() submitEvent = new EventEmitter<void>();
+  @Output() cancelEvent = new EventEmitter<void>();
+  @Input({ required: true }) formGroup!: FormGroup;
+  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) subtitle: string = '';
+  @Input({ required: true }) icon: string = '';
+}
